6d69c4b225d1e7c2afa847fe2c998a2f9e3aae89
JonasForssell <jonasforssell@yahoo.se>
2018-02-26 21:04:43 +0100

    Update tetra.py

--- klippy/tetra.py
+++ klippy/tetra.py
@@ -337,7 +337,7 @@ class TetraKinematics:
                 
                 # Calculate the effector position
                 previous_pos_r = current_pos_r
-                current_pos_r = self._movement_position_from_stepper_pos(beyond_reversal_point, move.axes_d, anchor_d, current_stepper_pos)
+                current_pos_r = self._movement_position_from_stepper_pos(beyond_reversal_point, axes_d, anchor_d, current_stepper_pos)
                 
                 # Calculate corresponding time depending on which phase we are in
                 if current_pos_r > (accel_d + cruise_d):                                             
