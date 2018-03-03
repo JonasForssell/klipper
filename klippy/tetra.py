81820e6f45a2ea2005a3bb4f01f33b664b4d8c2d
JonasForssell <jonasforssell@yahoo.se>
2018-03-03 13:11:03 +0100

    Update tetra.py

--- klippy/tetra.py
+++ klippy/tetra.py
@@ -400,6 +400,9 @@ class TetraKinematics:
         Vy = V[1]
         Vz = V[2]        
         
+        if (Vx**2+Vy**2+Vz**2) == 0:
+            return 0
+        
         if beyond_reversal_point:
             return (0.5*math.sqrt( (-2*PAx*Vx - 2*PAy*Vy - 2*PAz*Vz)**2 -4*(Vx**2+Vy**2+Vz**2)*(PAx**2+PAy**2+PAz**2 - current_stepper_pos**2)) 
                    + PAx*Vx + PAy*Vy + PAz*Vz ) / (Vx**2 + Vy**2 + Vz**2)
