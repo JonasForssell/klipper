834232b755db493d804c8e1f1e19246415543701
JonasForssell <jonasforssell@yahoo.se>
2018-03-04 16:12:42 +0100

    Update tetra.py

--- klippy/tetra.py
+++ klippy/tetra.py
@@ -360,6 +360,10 @@ class TetraKinematics:
                 previous_pos_r = current_pos_r
                 current_pos_r = self._movement_position_from_stepper_pos(beyond_reversal_point, axes_d, anchor_d, current_stepper_pos)
                 
+                logging.info(
+                    "current_pos_r: %.2f "
+                    % (current_pos_r))
+                
                 # Calculate corresponding time depending on which phase we are in
                 if current_pos_r > (accel_d + cruise_d):                                             
                     move_time += math.sqrt((current_pos_r-previous_pos_r)*move_d/decel)                                         
