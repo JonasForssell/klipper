78b46c979ad634b4f9b0a3ce6e6d71680a78bfcc
JonasForssell <jonasforssell@yahoo.se>
2018-03-04 16:34:21 +0100

    Update tetra.py

--- klippy/tetra.py
+++ klippy/tetra.py
@@ -431,8 +431,10 @@ class TetraKinematics:
             % (PAx, PAy, PAz, Vx, Vy, Vz, current_stepper_pos))
             
         logging.info(
-            "Del1: %.2f Del2: %.2f Del3: %.2f Del4: %.2f"
-            % ((-2*PAx*Vx - 2*PAy*Vy - 2*PAz*Vz)**2, -4*(Vx**2+Vy**2+Vz**2)*(PAx**2+PAy**2+PAz**2 - current_stepper_pos**2), PAx*Vx + PAy*Vy + PAz*Vz, (Vx**2 + Vy**2 + Vz**2)))
+            "_mpfsp1: %.2f _mpfsp2: %.2f"
+            % ((0.5*math.sqrt( (-2*PAx*Vx - 2*PAy*Vy - 2*PAz*Vz)**2 -4*(Vx**2+Vy**2+Vz**2)*(PAx**2+PAy**2+PAz**2 - current_stepper_pos**2)) 
+                   + PAx*Vx + PAy*Vy + PAz*Vz ) / (Vx**2 + Vy**2 + Vz**2), (-0.5*math.sqrt( (-2*PAx*Vx - 2*PAy*Vy - 2*PAz*Vz)**2 -4*(Vx**2+Vy**2+Vz**2)*(PAx**2+PAy**2+PAz**2 - current_stepper_pos**2)) 
+                   + PAx*Vx + PAy*Vy + PAz*Vz ) / (Vx**2 + Vy**2 + Vz**2))
         
         if beyond_reversal_point:
             return (0.5*math.sqrt( (-2*PAx*Vx - 2*PAy*Vy - 2*PAz*Vz)**2 -4*(Vx**2+Vy**2+Vz**2)*(PAx**2+PAy**2+PAz**2 - current_stepper_pos**2)) 
